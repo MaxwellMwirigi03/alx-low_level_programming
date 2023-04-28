0c658d924497b0284b408368a720c1943d16d11a
MaxwellMwirigi03 <maxwellmwithaliii@gmail.com>
2023-04-28 13:25:50 -0700

    Fullpush

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,17 +12,22 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 
 {
-	char *buffer;
-	ssize_t fd, w, t;
+	char *buffer = malloc(letters + 1);
+	int fd, w, fr;
 
-	fd = open(filename, O_RDONLY);
-	t = read(fd, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, t);
-	if (fd == -1)
+	if(!filename)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-
-	free(buffer);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0 || !buffer)
+	{
+		close(fd);
+		return (0);
+	}
+	fr = read(fd, buffer, letters);
 	close(fd);
-	return (w);
+	w = write(STDOUT_FILENO, buffer, fr);
+	if (w != fr || w == -1 || fr == -1)
+		return (0);
+	else
+		return (fr);
 }
