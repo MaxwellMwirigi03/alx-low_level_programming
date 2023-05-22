f2038396551cfc821efdd718803b1ebf048f4a1e
MaxwellMwirigi03 <maxwellmwithaliii@gmail.com>
2023-05-22 13:32:05 -0700

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check if a number is a digit
+ * @x: The number to be checked
+ * Return: 1 for a character that will be a digit or 0 for any other
+ */
+
+int _isdigit(int x)
+{
+	if (x >= '0' && x <= '9')
+		return (1);
+	else
+		return (0);
+}
